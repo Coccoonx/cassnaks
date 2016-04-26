26b17423ab1aff3d762c9646a8d7efa83a532b98
Edwidge <edwidge.tchounga@wouri.co>
2016-04-26 22:15:36 +0100

    Customize style of PinView and define its behavior

--- app/src/main/java/co/wouri/libreexchange/uis/LoginScreenActivity.java
+++ app/src/main/java/co/wouri/libreexchange/uis/LoginScreenActivity.java
@@ -1,35 +1,24 @@
 package co.wouri.libreexchange.uis;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.kbeanie.pinscreenlibrary.views.PinEntryAuthenticationListener;
 import com.kbeanie.pinscreenlibrary.views.PinEntrySetupListener;
 import com.kbeanie.pinscreenlibrary.views.PinView;
-
 import co.wouri.libreexchange.R;
-import co.wouri.libreexchange.api.ServerUtils;
-
-import co.wouri.libreexchange.core.managers.ProfileManager;
-import co.wouri.libreexchange.core.models.Customer;
-import co.wouri.libreexchange.storage.LibreExchangeSettingsUtils;
-import co.wouri.libreexchange.utils.FormValidationUtils;
 import co.wouri.libreexchange.utils.LoadingTask.LoadingTaskFinishedListener;
 import co.wouri.libreexchange.utils.UIUtils;
 
-public class LoginScreenActivity extends Activity implements LoadingTaskFinishedListener, PinEntryAuthenticationListener {
+public class LoginScreenActivity extends Activity implements LoadingTaskFinishedListener, PinEntryAuthenticationListener, PinEntrySetupListener {
 
     private static final String TAG = "LoginScreenActivity";
     TextView appName;
@@ -37,13 +26,15 @@ public class LoginScreenActivity extends Activity implements LoadingTaskFinished
     ImageView menu;
     EditText email;
     EditText password;
-    EditText passwordConf;
     PinView pinView;
     TextView loginWithEmailOrPin;
     TextView signUp;
-    LinearLayout loginForm;
+    RelativeLayout loginForm;
+    LinearLayout loginChoices;
     EditText firstName;
     EditText lastName;
+
+    private boolean inSignUpForm = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,18 +55,20 @@ public class LoginScreenActivity extends Activity implements LoadingTaskFinished
         firstName = (EditText) findViewById(R.id.userFirstName);
         lastName = (EditText) findViewById(R.id.userLastName);
 
-        UIUtils.setFont(UIUtils.Font.MUSEOSANS_500, appName, slogan, email, password,firstName,lastName);
         pinView = (PinView) findViewById(R.id.pinView);
         loginWithEmailOrPin = (TextView) findViewById(R.id.loginEmail);
         signUp = (TextView) findViewById(R.id.signUp);
 
-        loginForm = (LinearLayout) findViewById(R.id.loginForm);
+        loginForm = (RelativeLayout) findViewById(R.id.loginForm);
+        loginChoices = (LinearLayout) findViewById(R.id.loginChoices) ;
 
         pinView.setModeAuthenticate(this);
 
+        UIUtils.setFont(UIUtils.Font.MUSEOSANS_500, appName, slogan, email, password,firstName,lastName);
         loginWithEmailOrPin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                inSignUpForm = false;
                 if(loginWithEmailOrPin.getText().equals("Log in with email ")){
                     loginWithEmailOrPin.setText("Log in with pin ");
                     pinView.setVisibility(View.GONE);
@@ -89,18 +82,49 @@ public class LoginScreenActivity extends Activity implements LoadingTaskFinished
             }
         });
 
-        UIUtils.setFont(UIUtils.Font.MUSEOSANS_500, appName, slogan, email, password, passwordConf);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inSignUpForm = true;
+                pinView.setVisibility(View.GONE);
+                loginChoices.setVisibility(View.GONE);
+                loginForm.setVisibility(View.VISIBLE);
+                firstName.setVisibility(View.VISIBLE);
+                lastName.setVisibility(View.VISIBLE);
+            }
+        });
 
+        UIUtils.setFont(UIUtils.Font.MUSEOSANS_500, appName, slogan, email, password);
+
+    }
+
+    @Override
+    public void onPinEntered(String pin) {
+
+    }
+
+    @Override
+    public void onPinConfirmed(String pin) {
+
+    }
+
+    @Override
+    public void onPinMismatch() {
+
+    }
+
+    @Override
+    public void onPinSet(String pin) {
+        startApp();
     }
 
     @Override
     public void onPinCorrect() {
-        finish();
+        startApp();
     }
 
     @Override
     public void onPinWrong() {
-
     }
 
     // This is the callback for when your async task has finished
@@ -128,43 +152,38 @@ public class LoginScreenActivity extends Activity implements LoadingTaskFinished
     }
 
     private void startApp() {
-        Intent intent;
-//        if (!LibreExchangeSettingsUtils.getUserEmail().equals("")) {
-        intent = new Intent(LoginScreenActivity.this, MainActivity.class);
-//        } else
-//            intent = new Intent(LoginScreenActivity.this, ProfileActivity.class);
+        Intent intent = new Intent(LoginScreenActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
     public void performSubmit(View view) {
-//        startApp();
-        String emailVal = email.getText().toString().trim();
-        String passwordVal = password.getText().toString();
-        String firstName = email.getText().toString().trim();
-        String lastName = password.getText().toString();
-//        String country = this.getResources().getConfiguration().locale.getCountry();
-        String country = "CA";
-//        TelephonyManager tMgr = (TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
-//        String mPhoneNumber = tMgr.getLine1Number();
-//        if (FormValidationUtils.checkEmail(emailVal)) {
-//            if (passwordVal.equals(passwordCVal)) {
-        Customer userCustomer = new Customer();
-        userCustomer.setEmail(emailVal);
-                userCustomer.setPassword(passwordVal);
-        userCustomer.setCountry(country);
-        userCustomer.setFirstName(firstName);
-        userCustomer.setLastName(lastName);
-                Customer customer = ServerUtils.createCustomer(this, userCustomer);
-                Log.d(TAG, "Customer created: "+customer);
-                if (customer != null) {
-                    ProfileManager.getCurrentUserProfile().setCustomer(customer);
-                    ProfileManager.saveProfile();
-                    LibreExchangeSettingsUtils.setUserEmail(emailVal);
-                    startApp();
-                }
-//            } else
-//                Toast.makeText(LoginScreenActivity.this, "Password doesn't match", Toast.LENGTH_SHORT).show();
-//        } else
-//            Toast.makeText(LoginScreenActivity.this, "Invalid Email Address", Toast.LENGTH_SHORT).show();
+        loginForm.setVisibility(View.GONE);
+        pinView.setVisibility(View.VISIBLE);
+        if(inSignUpForm) {
+            pinView.setModeSetup(this);
+        }else{
+            // Test if Customer exists in the database and then logged in
+            startApp();
+        }
+
+//        String emailVal = email.getText().toString().trim();
+//        String passwordVal = password.getText().toString();
+//        String firstName = email.getText().toString().trim();
+//        String lastName = password.getText().toString();
+//        String country = "CA";
+//        Customer userCustomer = new Customer();
+//        userCustomer.setEmail(emailVal);
+//        userCustomer.setPassword(passwordVal);
+//        userCustomer.setCountry(country);
+//        userCustomer.setFirstName(firstName);
+//        userCustomer.setLastName(lastName);
+//        Customer customer = ServerUtils.createCustomer(this, userCustomer);
+//        Log.d(TAG, "Customer created: "+customer);
+//        if (customer != null) {
+//            ProfileManager.getCurrentUserProfile().setCustomer(customer);
+//            ProfileManager.saveProfile();
+//            LibreExchangeSettingsUtils.setUserEmail(emailVal);
+//            startApp();
+//        }
     }
 }
